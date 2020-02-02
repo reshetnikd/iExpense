aa70e5d132e2ad36010be67d5a8b5735e0d59e1d
reshetnikd <dmitry.reshetnik@icloud.com>
2020-02-02 13:22:49 +0200

    Add UI including second View with dynamic List and dismiss button

--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -8,9 +8,67 @@
 
 import SwiftUI
 
-struct ContentView: View {
+class User: ObservableObject {
+    @Published var firstName = "Bilbo"
+    @Published var lastName = "Baggins"
+}
+
+struct SecondView: View {
+    
+    @Environment(\.presentationMode) var presentationMode
+    @State private var numbers = [Int]()
+    @State private var currentNumber = 1
+    var name: String
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            VStack {
+                Text("Hello, \(name)!")
+                
+                List {
+                    ForEach(numbers, id: \.self) {
+                        Text("\($0)")
+                    }
+                    .onDelete(perform: removeRows)
+                }
+                
+                Button("Add Number") {
+                    self.numbers.append(self.currentNumber)
+                    self.currentNumber += 1
+                }
+                
+                Button("Dismiss") {
+                    self.presentationMode.wrappedValue.dismiss()
+                }
+            }
+            .navigationBarItems(leading: EditButton())
+        }
+    }
+    
+    func removeRows(at offsets: IndexSet) {
+        numbers.remove(atOffsets: offsets)
+    }
+}
+
+struct ContentView: View {
+    
+    @ObservedObject var user = User()
+    @State private var showingSheet = false
+    
+    var body: some View {
+        VStack {
+            Text("Your name is \(user.firstName) \(user.lastName).")
+            
+            TextField("First name", text: $user.firstName)
+            TextField("Last name", text: $user.lastName)
+            
+            Button("Show Sheet") {
+                self.showingSheet.toggle()
+            }
+            .sheet(isPresented: $showingSheet) {
+                SecondView(name: self.user.firstName)
+            }
+        }
     }
 }
 
